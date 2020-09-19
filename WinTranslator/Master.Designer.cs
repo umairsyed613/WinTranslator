3ce242717981d4d22f46b3d64357801bb024ac83
Administrator <Administrator@LAPTOP-NJPU8J9A>
2020-09-19 02:23:32 +0200

    project setup

---
+++ WinTranslator/Master.Designer.cs
@@ -0,0 +1,124 @@
+﻿namespace WinTranslator
+{
+    partial class Master
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.simpleLogTextBox1 = new Serilog.Sinks.WinForms.SimpleLogTextBox();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(10, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(30, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "From";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(10, 30);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(355, 190);
+            this.textBox1.TabIndex = 1;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(381, 30);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(355, 190);
+            this.textBox2.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(383, 13);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(20, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "To";
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.White;
+            this.panel1.Controls.Add(this.textBox1);
+            this.panel1.Controls.Add(this.textBox2);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Location = new System.Drawing.Point(12, 30);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(760, 249);
+            this.panel1.TabIndex = 4;
+            this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
+            // 
+            // simpleLogTextBox1
+            // 
+            this.simpleLogTextBox1.Location = new System.Drawing.Point(13, 286);
+            this.simpleLogTextBox1.LogBorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.simpleLogTextBox1.LogPadding = new System.Windows.Forms.Padding(3);
+            this.simpleLogTextBox1.Name = "simpleLogTextBox1";
+            this.simpleLogTextBox1.ReadOnly = true;
+            this.simpleLogTextBox1.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.simpleLogTextBox1.Size = new System.Drawing.Size(759, 122);
+            this.simpleLogTextBox1.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 420);
+            this.Controls.Add(this.simpleLogTextBox1);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "WinTranslater";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Panel panel1;
+        private Serilog.Sinks.WinForms.SimpleLogTextBox simpleLogTextBox1;
+    }
+}
+
---
+++ WinTranslator/Master.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Gma.System.MouseKeyHook;
+using Serilog;
+
+namespace WinTranslator
+{
+    public partial class Master : Form
+    {
+        public Master()
+        {
+            InitializeComponent();
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+            Graphics g = e.Graphics;
+            g.SmoothingMode = SmoothingMode.AntiAlias;
+            g.FillRoundedRectangle(new SolidBrush(Color.White), 20, 20, this.Width - 40, this.Height - 60, 30);
+            SolidBrush brush = new SolidBrush(
+                Color.White
+            );
+            g.FillRoundedRectangle(brush, 12, 12, this.Width - 44, this.Height - 64, 10);
+            g.DrawRoundedRectangle(new Pen(ControlPaint.Light(Color.White, 0.00f)), 12, 12, this.Width - 44,
+                this.Height - 64, 10);
+            g.FillRoundedRectangle(new SolidBrush(Color.White), 12, 12 + ((this.Height - 64) / 2), this.Width - 44,
+                (this.Height - 64) / 2, 10);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Log.Information("Setting up the hook");
+            HookToKeyboard();
+        }
+
+        private void HookToKeyboard()
+        {
+            Hook.GlobalEvents().KeyDown += async (s, e) =>
+            {
+                if (e.Control && e.Shift && e.KeyCode == Keys.T)
+                {
+                    Log.Information("Sending command to keyboard to copy text.");
+                    KeyboardSend.KeyDown(Keys.Control);
+                    KeyboardSend.KeyDown(Keys.C);
+
+                    
+                    ReadFromClipboard();
+                }
+            };
+        }
+
+        private void ReadFromClipboard()
+        {
+            Log.Information("Readying from clipboard.");
+            if (Clipboard.ContainsText())
+            {
+                textBox1.Text = Clipboard.GetText();
+            }
+        }
+    }
+}
